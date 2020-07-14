a9f4a137cf0e49e71fd3fddc1042c715611d7bb7
utkarsh22040 <54702807+utkarsh22040@users.noreply.github.com>
2020-07-14 13:09:59 +0530

    Update SpeakGoodBye.js

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -18,7 +18,7 @@
 // STEP 8: Rewrite the 'speak' function such that it is attached to the
 // byeSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
-function speak(byeSpeaker.name) {
+function byeSpeaker.speak(byeSpeaker.name) {
   console.log(speakWord + " " + byeSpeaker.name);
 }
   window.byeSpeaker = byeSpeaker;
